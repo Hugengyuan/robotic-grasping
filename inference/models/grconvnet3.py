48cd970c90caca2ae1cd2231a896183ce6e2aba8
cayman <hugengyuan@outlook.com>
2022-11-09 14:08:21 +0800

    architecture changed

--- inference/models/grconvnet3.py
+++ inference/models/grconvnet3.py
@@ -31,7 +31,7 @@ class GenerativeResnet(GraspModel):
         self.conv5 = nn.ConvTranspose2d(channel_size * 2, channel_size, kernel_size=4, stride=2, padding=1)
         self.bn5 = nn.BatchNorm2d(channel_size)
 
-        self.down3 = nn.Conv2d(channel_size * 4, channel_size, kernel_size=3, stride=1, padding=1)
+        self.down3 = nn.Conv2d(channel_size * 2, channel_size, kernel_size=3, stride=1, padding=1)
         self.conv6 = nn.ConvTranspose2d(channel_size, channel_size, kernel_size=10, stride=1, padding=4)
 
         self.pos_output = nn.Conv2d(in_channels=channel_size, out_channels=output_channels, kernel_size=2)
