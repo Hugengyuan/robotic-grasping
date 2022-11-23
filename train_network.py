1d53ddc941d48bf074f957226853e639c0f5193e
Cayman <56817261+Hugengyuan@users.noreply.github.com>
2022-11-23 23:09:34 +0800

    Update train_network.py

--- train_network.py
+++ train_network.py
@@ -12,6 +12,7 @@ import torch
 import torch.optim as optim
 import torch.utils.data
 from torchsummary import summary
+from sklearn.model_selection import KFold
 
 from hardware.device import get_device
 from inference.models import get_network
@@ -259,19 +260,32 @@ def run():
                       include_rgb=args.use_rgb)
     logging.info('Dataset size is {}'.format(dataset.length))
 
+    
     # Creating data indices for training and validation splits
     indices = list(range(dataset.length))
-    split = int(np.floor(args.split * dataset.length))
-    if args.ds_shuffle:
-        np.random.seed(args.random_seed)
-        np.random.shuffle(indices)
-    train_indices, val_indices = indices[:split], indices[split:]
-    logging.info('Training size: {}'.format(len(train_indices)))
-    logging.info('Validation size: {}'.format(len(val_indices)))
+#     split = int(np.floor(args.split * dataset.length))
+#     if args.ds_shuffle:
+#         np.random.seed(args.random_seed)
+#         np.random.shuffle(indices)
+#     train_indices, val_indices = indices[:split], indices[split:]
+    
+    kf = KFold(n_splits=5, random_state=42, shuffle=True)  # 初始化KFold
+    for train_indices , val_indices in kf.split(indices):  # 调用split方法切分数据
+        logging.info('train_index:%s {}'.format(len(train_indices)))
+        logging.info('test_index:%s {}'.format(len(val_indices)))
+    
+    train_files = []   # 存放5折的训练集划分
+    test_files = []     # # 存放5折的测试集集划分
+    for k, (Trindex, Tsindex) in enumerate(kf.split(indices)):
+        train_files.append(np.array(indices)[Trindex].tolist())
+        test_files.append(np.array(indices)[Tsindex].tolist())
+
+#     logging.info('Training size: {}'.format(len(train_indices[0])))
+#     logging.info('Validation size: {}'.format(len(val_indices[0])))
 
     # Creating data samplers and loaders
-    train_sampler = torch.utils.data.sampler.SubsetRandomSampler(train_indices)
-    val_sampler = torch.utils.data.sampler.SubsetRandomSampler(val_indices)
+    train_sampler = torch.utils.data.sampler.SubsetRandomSampler(train_files[0])
+    val_sampler = torch.utils.data.sampler.SubsetRandomSampler(test_files[0])
 
     train_data = torch.utils.data.DataLoader(
         dataset,
