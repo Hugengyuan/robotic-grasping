a4199db2c7ae5a2bb91f97b13797e066f792bb85
Cayman <56817261+Hugengyuan@users.noreply.github.com>
2023-02-28 14:39:02 +0800

    Update grconvnet3.py

--- inference/models/grconvnet3.py
+++ inference/models/grconvnet3.py
@@ -8,28 +8,33 @@ class GenerativeResnet(GraspModel):
 
     def __init__(self, input_channels=4, output_channels=1, channel_size=32, dropout=False, prob=0.0):
         super(GenerativeResnet, self).__init__()
-        self.conv1 = nn.Conv2d(input_channels, channel_size, kernel_size=9, stride=1, padding=4)
-        self.bn1 = nn.BatchNorm2d(channel_size)
+        self.conv1 = nn.Conv2d(input_channels, channel_size * 2, kernel_size=9, stride=1, padding=4)
+        self.bn1 = nn.BatchNorm2d(channel_size * 2)
 
-        self.conv2 = nn.Conv2d(channel_size, channel_size * 2, kernel_size=4, stride=2, padding=1)
-        self.bn2 = nn.BatchNorm2d(channel_size * 2)
+        self.conv2 = nn.Conv2d(channel_size, channel_size * 4, kernel_size=4, stride=2, padding=1)
+        self.bn2 = nn.BatchNorm2d(channel_size * 4)
 
-        self.conv3 = nn.Conv2d(channel_size * 2, channel_size * 4, kernel_size=4, stride=2, padding=1)
-        self.bn3 = nn.BatchNorm2d(channel_size * 4)
+        self.conv3 = nn.Conv2d(channel_size * 4, channel_size * 8, kernel_size=4, stride=2, padding=1)
+        self.bn3 = nn.BatchNorm2d(channel_size * 8)
+        
+        self.conv4 = nn.Conv2d(channel_size * 8, channel_size * 16, kernel_size=4, stride=2, padding=1)
+        self.bn4 = nn.BatchNorm2d(channel_size * 16)
 
-        self.res1 = ResidualBlock(channel_size * 4, channel_size * 4, dilation=1)
-        self.res2 = ResidualBlock(channel_size * 4, channel_size * 4, dilation=2)
-        self.res3 = ResidualBlock(channel_size * 4, channel_size * 4, dilation=4)
-        self.res4 = ResidualBlock(channel_size * 4, channel_size * 4, dilation=8)
-        self.res5 = ResidualBlock(channel_size * 4, channel_size * 4, dilation=16)
+        self.res1 = ResidualBlock(channel_size * 16, channel_size * 16, dilation=2)
+        self.res2 = ResidualBlock(channel_size * 16, channel_size * 16, dilation=4)
+        self.res3 = ResidualBlock(channel_size * 16, channel_size * 16, dilation=6)
 
-        self.conv4 = nn.ConvTranspose2d(channel_size * 4, channel_size * 2, kernel_size=4, stride=2, padding=1)
-        self.bn4 = nn.BatchNorm2d(channel_size * 2)
 
-        self.conv5 = nn.ConvTranspose2d(channel_size * 2, channel_size, kernel_size=4, stride=2, padding=1)
-        self.bn5 = nn.BatchNorm2d(channel_size)
+        self.conv5 = nn.ConvTranspose2d(channel_size * 16, channel_size * 8, kernel_size=4, stride=2, padding=1)
+        self.bn5 = nn.BatchNorm2d(channel_size * 8)
+        
+        self.conv6 = nn.ConvTranspose2d(channel_size * 8, channel_size * 4, kernel_size=4, stride=2, padding=1)
+        self.bn6 = nn.BatchNorm2d(channel_size * 4)
 
-        self.conv6 = nn.ConvTranspose2d(channel_size, channel_size, kernel_size=9, stride=1, padding=4)
+        self.conv7 = nn.ConvTranspose2d(channel_size * 4, channel_size * 2, kernel_size=4, stride=2, padding=1)
+        self.bn7 = nn.BatchNorm2d(channel_size * 2)
+
+        self.conv8 = nn.ConvTranspose2d(channel_size * 2, channel_size, kernel_size=9, stride=1, padding=4)
 
         self.pos_output = nn.Conv2d(in_channels=channel_size, out_channels=output_channels, kernel_size=1)
         self.cos_output = nn.Conv2d(in_channels=channel_size, out_channels=output_channels, kernel_size=1)
@@ -51,14 +56,14 @@ class GenerativeResnet(GraspModel):
         x = F.relu(self.bn1(self.conv1(x_in)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.bn3(self.conv4(x)))
         x = self.res1(x)
         x = self.res2(x)
         x = self.res3(x)
-        x = self.res4(x)
-        x = self.res5(x)
-        x = F.relu(self.bn4(self.conv4(x)))
         x = F.relu(self.bn5(self.conv5(x)))
-        x = self.conv6(x)
+        x = F.relu(self.bn6(self.conv6(x)))
+        x = F.relu(self.bn7(self.conv7(x)))
+        x = self.conv8(x)
 
         if self.dropout:
             pos_output = self.pos_output(self.dropout_pos(x))
