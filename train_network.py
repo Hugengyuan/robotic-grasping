a7c51f39d539c91e193e71d47f1486b2ad49016b
Cayman <56817261+Hugengyuan@users.noreply.github.com>
2023-02-28 17:40:33 +0800

    Update train_network.py

--- train_network.py
+++ train_network.py
@@ -331,7 +331,7 @@ def run():
     summary(net, [(input_channels, args.input_size, args.input_size), (1, args.input_size, args.input_size)])
     f = open(os.path.join(save_folder, 'arch.txt'), 'w')
     sys.stdout = f
-    summary(net, [(input_channels, 1, args.input_size, args.input_size), (1, args.input_size, args.input_size)])
+    summary(net, [(input_channels, args.input_size, args.input_size), (1, args.input_size, args.input_size)])
     sys.stdout = sys.__stdout__
     f.close()
     
