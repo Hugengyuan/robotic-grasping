b8e15145a0f8bc9cdcafd96c93d135a5ae1603f2
Cayman <56817261+Hugengyuan@users.noreply.github.com>
2023-03-01 17:52:23 +0800

    Update grconvnet3.py

--- inference/models/grconvnet3.py
+++ inference/models/grconvnet3.py
@@ -19,6 +19,9 @@ class GenerativeResnet(GraspModel):
         self.rgb_conv3 = nn.Conv2d(channel_size * 4, channel_size * 8, kernel_size=4, stride=2, padding=1)
         self.rgb_bn3 = nn.BatchNorm2d(channel_size * 8)
         
+        self.rgb_conv4 = nn.Conv2d(channel_size * 8, channel_size * 16, kernel_size=4, stride=2, padding=1)
+        self.rgb_bn4 = nn.BatchNorm2d(channel_size * 16)
+        
         self.depth_conv1 = nn.Conv2d(input_channels, channel_size * 2, kernel_size=9, stride=1, padding=4)
         self.depth_bn1 = nn.BatchNorm2d(channel_size * 2)
 
@@ -27,6 +30,9 @@ class GenerativeResnet(GraspModel):
 
         self.depth_conv3 = nn.Conv2d(channel_size * 4, channel_size * 8, kernel_size=4, stride=2, padding=1)
         self.depth_bn3 = nn.BatchNorm2d(channel_size * 8)
+        
+        self.depth_conv4 = nn.Conv2d(channel_size * 8, channel_size * 16, kernel_size=4, stride=2, padding=1)
+        self.depth_bn4 = nn.BatchNorm2d(channel_size * 16)
         
         # 多层感知机的定义
         # self.fc1 = nn.Linear(in_features=3*224*224, out_features=4096)
@@ -37,6 +43,8 @@ class GenerativeResnet(GraspModel):
         self.res1 = ResidualBlock(channel_size * 16, channel_size * 16)
         self.res2 = ResidualBlock(channel_size * 16, channel_size * 16)
         self.res3 = ResidualBlock(channel_size * 16, channel_size * 16)
+        self.res4 = ResidualBlock(channel_size * 16, channel_size * 16)
+        self.res5 = ResidualBlock(channel_size * 16, channel_size * 16)
 
         self.up_conv1 = nn.ConvTranspose2d(channel_size * 16, channel_size * 8, kernel_size=4, stride=2, padding=1)
         self.up_bn1 = nn.BatchNorm2d(channel_size * 8)
@@ -66,15 +74,20 @@ class GenerativeResnet(GraspModel):
         rgb_x = F.relu(self.rgb_bn1(self.rgb_conv1(rgb_in)))
         rgb_x = F.relu(self.rgb_bn2(self.rgb_conv2(rgb_x)))
         rgb_x = F.relu(self.rgb_bn3(self.rgb_conv3(rgb_x)))
+        rgb_x = F.relu(self.rgb_bn4(self.rgb_conv4(rgb_x)))
         depth_x = depth_in.repeat_interleave(3,1)
         depth_x = F.relu(self.depth_bn1(self.depth_conv1(depth_x)))
         depth_x = F.relu(self.depth_bn2(self.depth_conv2(depth_x)))
         depth_x = F.relu(self.depth_bn3(self.depth_conv3(depth_x)))
+        depth_x = F.relu(self.depth_bn4(self.depth_conv4(depth_x)))
         
-        x = torch.cat([rgb_x, depth_x], 1)
+        #x = torch.cat([rgb_x, depth_x], 1)
+        x = rgb_x + depth_x
         x = self.res1(x)
         x = self.res2(x)
         x = self.res3(x)
+        x = self.res4(x)
+        x = self.res5(x)
        
         x = F.relu(self.up_bn1(self.up_conv1(x)))
         x = F.relu(self.up_bn2(self.up_conv2(x)))
