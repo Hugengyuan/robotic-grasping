e651200de89ab4bc04d7a1d2c92975f5926c7842
Cayman <56817261+Hugengyuan@users.noreply.github.com>
2023-04-03 21:20:04 +0800

    Update irnn.py

--- inference/models/irnn.py
+++ inference/models/irnn.py
@@ -31,17 +31,17 @@ class irnn(torch.autograd.Function):
     @staticmethod
     def forward(self, input_feature, weight_up, weight_right, weight_down, weight_left, bias_up, bias_right, bias_down,
                 bias_left):
-        input_feature.as_contiguous()
-        assert (input_feature.is_contiguous() == True)
-        assert (weight_left.is_contiguous() == True)
-        assert (weight_right.is_contiguous() == True)
-        assert (weight_down.is_contiguous() == True)
+        
+#         assert (input_feature.is_contiguous() == True)
+#         assert (weight_left.is_contiguous() == True)
+#         assert (weight_right.is_contiguous() == True)
+#         assert (weight_down.is_contiguous() == True)
 
-        assert (weight_up.is_contiguous() == True)
-        assert (bias_left.is_contiguous() == True)
-        assert (bias_right.is_contiguous() == True)
-        assert (bias_up.is_contiguous() == True)
-        assert (bias_down.is_contiguous() == True)
+#         assert (weight_up.is_contiguous() == True)
+#         assert (bias_left.is_contiguous() == True)
+#         assert (bias_right.is_contiguous() == True)
+#         assert (bias_up.is_contiguous() == True)
+#         assert (bias_down.is_contiguous() == True)
 
         output_left = input_feature.clone()
         output_right = input_feature.clone()
