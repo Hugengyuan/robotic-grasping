d8cc7f749eb08e35c79509f7e11d1472c534a5ae
Cayman <56817261+Hugengyuan@users.noreply.github.com>
2023-04-05 19:54:42 +0800

    Update DSC.py

--- inference/models/DSC.py
+++ inference/models/DSC.py
@@ -64,9 +64,9 @@ class SEWeightModule(nn.Module):
     def __init__(self, channels, reduction=16):
         super(SEWeightModule, self).__init__()
         self.avg_pool = nn.AdaptiveAvgPool2D(1)
-        self.fc1 = nn.Conv2D(channels, channels//reduction, kernel_size=1, padding=0)
+        self.fc1 = nn.Conv2d(channels, channels//reduction, kernel_size=1, padding=0)
         self.relu = nn.ReLU()
-        self.fc2 = nn.Conv2D(channels//reduction, channels, kernel_size=1, padding=0)
+        self.fc2 = nn.Conv2d(channels//reduction, channels, kernel_size=1, padding=0)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
