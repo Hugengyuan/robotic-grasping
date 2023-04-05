22e3eea01b7dd8ffa11fa19584b707517d06df14
Cayman <56817261+Hugengyuan@users.noreply.github.com>
2023-04-05 20:19:24 +0800

    Update DSC.py

--- inference/models/DSC.py
+++ inference/models/DSC.py
@@ -58,14 +58,19 @@ def conv(in_planes, out_planes, kernel_size=3, stride=1, padding=1, dilation=1, 
     return nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride,
                      padding=padding, dilation=dilation, groups=groups, bias=False)
 
+def conv(in_planes, out_planes, kernel_size=3, stride=1, padding=1, dilation=1, groups=1):
+    """standard convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride,
+                     padding=padding, dilation=dilation, groups=groups, bias=False)
+
 
 class SEWeightModule(nn.Module):
 
     def __init__(self, channels, reduction=16):
         super(SEWeightModule, self).__init__()
-        self.avg_pool = nn.AdaptiveAvgPool2D(1)
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.fc1 = nn.Conv2d(channels, channels//reduction, kernel_size=1, padding=0)
-        self.relu = nn.ReLU()
+        self.relu = nn.ReLU(inplace=True)
         self.fc2 = nn.Conv2d(channels//reduction, channels, kernel_size=1, padding=0)
         self.sigmoid = nn.Sigmoid()
 
@@ -93,39 +98,36 @@ class PSAModule(nn.Module):
                             stride=stride, groups=conv_groups[3])
         self.se = SEWeightModule(planes // 4)
         self.split_channel = planes // 4
-        self.softmax = nn.Softmax(axis=1)
+        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
-        # stage 1
         batch_size = x.shape[0]
         x1 = self.conv_1(x)
         x2 = self.conv_2(x)
         x3 = self.conv_3(x)
         x4 = self.conv_4(x)
 
-        feats = torch.concat((x1, x2, x3, x4), axis=1)
-        feats = feats.reshape([batch_size, 4, self.split_channel, feats.shape[2], feats.shape[3]])
-        
-        # stage 2
+        feats = torch.cat((x1, x2, x3, x4), dim=1)
+        feats = feats.view(batch_size, 4, self.split_channel, feats.shape[2], feats.shape[3])
+
         x1_se = self.se(x1)
         x2_se = self.se(x2)
         x3_se = self.se(x3)
         x4_se = self.se(x4)
 
-        x_se = paddle.concat((x1_se, x2_se, x3_se, x4_se), axis=1)
-        attention_vectors = x_se.reshape([batch_size, 4, self.split_channel, 1, 1])
-        attention_vectors = self.softmax(attention_vectors) # stage 3
-
-        # stage 4
+        x_se = torch.cat((x1_se, x2_se, x3_se, x4_se), dim=1)
+        attention_vectors = x_se.view(batch_size, 4, self.split_channel, 1, 1)
+        attention_vectors = self.softmax(attention_vectors)
         feats_weight = feats * attention_vectors
         for i in range(4):
             x_se_weight_fp = feats_weight[:, i, :, :]
             if i == 0:
                 out = x_se_weight_fp
             else:
-                out = torch.concat((x_se_weight_fp, out), axis=1)
+                out = torch.cat((x_se_weight_fp, out), 1)
 
         return out
+    
 
 class DSC_Module(nn.Module):
     def __init__(self, in_channels, out_channels, attention=0, alpha=1.0):
