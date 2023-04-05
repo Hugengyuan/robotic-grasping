4264db8ce17b201efd16725f3b18d9eacceacc90
Cayman <56817261+Hugengyuan@users.noreply.github.com>
2023-04-05 21:10:15 +0800

    Update grconvnet3.py

--- inference/models/grconvnet3.py
+++ inference/models/grconvnet3.py
@@ -28,7 +28,7 @@ class GenerativeResnet(GraspModel):
 
         self.conv5 = nn.ConvTranspose2d(channel_size * 4, channel_size  * 2, kernel_size=4, stride=2, padding=1)
         self.bn5 = nn.BatchNorm2d(channel_size  * 2)
-        self.conv6 = nn.ConvTranspose2d(channel_size * 4, channel_size, kernel_size=9, stride=1, padding=4)
+        self.conv6 = nn.ConvTranspose2d(channel_size * 4, channel_size, kernel_size=9, stride=1, padding=4, out_padding=1)
 
         self.pos_output = nn.Conv2d(in_channels=channel_size, out_channels=output_channels, kernel_size=2)
         self.cos_output = nn.Conv2d(in_channels=channel_size, out_channels=output_channels, kernel_size=2)
