c2a66c46d52c7a14d7b3777197b037f272f07282
Cayman <56817261+Hugengyuan@users.noreply.github.com>
2023-04-05 19:33:04 +0800

    Update DSC.py

--- inference/models/DSC.py
+++ inference/models/DSC.py
@@ -53,6 +53,79 @@ class Attention(nn.Module):
         out = self.sigmod(out)
         return out
 
+def conv(in_planes, out_planes, kernel_size=3, stride=1, padding=1, dilation=1, groups=1):
+    """standard convolution with padding"""
+    return nn.Conv2D(in_planes, out_planes, kernel_size=kernel_size, stride=stride,
+                     padding=padding, dilation=dilation, groups=groups, bias_attr=False)
+
+
+class SEWeightModule(nn.Layer):
+
+    def __init__(self, channels, reduction=16):
+        super(SEWeightModule, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2D(1)
+        self.fc1 = nn.Conv2D(channels, channels//reduction, kernel_size=1, padding=0)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Conv2D(channels//reduction, channels, kernel_size=1, padding=0)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        out = self.avg_pool(x)
+        out = self.fc1(out)
+        out = self.relu(out)
+        out = self.fc2(out)
+        weight = self.sigmoid(out)
+
+        return weight
+
+
+class PSAModule(nn.Layer):
+
+    def __init__(self, inplans, planes, conv_kernels=[3, 5, 7, 9], stride=1, conv_groups=[1, 4, 8, 16]):
+        super(PSAModule, self).__init__()
+        self.conv_1 = conv(inplans, planes//4, kernel_size=conv_kernels[0], padding=conv_kernels[0]//2,
+                            stride=stride, groups=conv_groups[0])
+        self.conv_2 = conv(inplans, planes//4, kernel_size=conv_kernels[1], padding=conv_kernels[1]//2,
+                            stride=stride, groups=conv_groups[1])
+        self.conv_3 = conv(inplans, planes//4, kernel_size=conv_kernels[2], padding=conv_kernels[2]//2,
+                            stride=stride, groups=conv_groups[2])
+        self.conv_4 = conv(inplans, planes//4, kernel_size=conv_kernels[3], padding=conv_kernels[3]//2,
+                            stride=stride, groups=conv_groups[3])
+        self.se = SEWeightModule(planes // 4)
+        self.split_channel = planes // 4
+        self.softmax = nn.Softmax(axis=1)
+
+    def forward(self, x):
+        # stage 1
+        batch_size = x.shape[0]
+        x1 = self.conv_1(x)
+        x2 = self.conv_2(x)
+        x3 = self.conv_3(x)
+        x4 = self.conv_4(x)
+
+        feats = paddle.concat((x1, x2, x3, x4), axis=1)
+        feats = feats.reshape([batch_size, 4, self.split_channel, feats.shape[2], feats.shape[3]])
+        
+        # stage 2
+        x1_se = self.se(x1)
+        x2_se = self.se(x2)
+        x3_se = self.se(x3)
+        x4_se = self.se(x4)
+
+        x_se = paddle.concat((x1_se, x2_se, x3_se, x4_se), axis=1)
+        attention_vectors = x_se.reshape([batch_size, 4, self.split_channel, 1, 1])
+        attention_vectors = self.softmax(attention_vectors) # stage 3
+
+        # stage 4
+        feats_weight = feats * attention_vectors
+        for i in range(4):
+            x_se_weight_fp = feats_weight[:, i, :, :]
+            if i == 0:
+                out = x_se_weight_fp
+            else:
+                out = paddle.concat((x_se_weight_fp, out), axis=1)
+
+        return out
 
 class DSC_Module(nn.Module):
     def __init__(self, in_channels, out_channels, attention=1, alpha=1.0):
@@ -61,12 +134,13 @@ class DSC_Module(nn.Module):
         self.irnn1 = Spacial_IRNN(self.out_channels, alpha)
         self.irnn2 = Spacial_IRNN(self.out_channels, alpha)
         self.conv_in = conv1x1(in_channels, in_channels)
-        self.conv2 = conv1x1(in_channels * 4, in_channels)
-        self.conv3 = conv1x1(in_channels * 4, in_channels)
+#         self.conv2 = conv1x1(in_channels * 4, in_channels)
+        self.conv2 = PSAModule(in_channels * 4, in_channels)
+        self.conv3 = PSAModule(in_channels * 4, in_channels)
         self.relu2 = nn.ReLU(True)
         self.attention = attention
         if self.attention:
-            self.attention_layer = Attention(in_channels)
+            self.attention_layer = 
 
     def forward(self, x):
         if self.attention:
